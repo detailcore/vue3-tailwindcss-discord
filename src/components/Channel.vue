0be4ba1e6b576897dbf6b697c8d5f9a9c70d8dd8
detailcore <savemysoulok@yandex.ru>
2023-10-19 18:04:02 +0400

    Update Channel.vue
    get channel by id

--- src/components/Channel.vue
+++ src/components/Channel.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="px-3 h-12 flex items-center shadow-sm">General</div>
+  <div class="px-3 h-12 flex items-center shadow-sm">{{ getChannel.label }}</div>
   <div class="p-3 flex-1 space-y-2 overflow-y-scroll">
   </div>
 </template>
 
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useRoute } from 'vue-router'
+import { useMainStore } from '@/stores'
+import { computed } from 'vue';
+import { IChannel } from '@/stores/index.interfaces';
+
+const route = useRoute()
+const { getServerCategories } = storeToRefs(useMainStore())
+
+const getChannel = computed<IChannel>(() => {
+  return getServerCategories.value
+    .map(c => c.channels)
+    .flat()
+    .find(ch => ch.id == +route.params.cid) as IChannel
+})
+
 </script>
