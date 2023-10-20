c9544d5c615c36fd5406ef0b9b58540100fea55f
detailcore <savemysoulok@yandex.ru>
2023-10-20 15:52:40 +0400

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+// const defaultTheme = require("tailwindcss/defaultTheme");
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,7 +8,7 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
     "./src/components/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
+  // darkMode: false, // or 'media' or 'class'
   // mode: 'jit',
   theme: {
     boxShadow: {
@@ -21,23 +22,23 @@ export default {
         title: ["Ginto", "Open Sans", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        brand: '#5865F2',
+        brand: "#5865F2",
         gray: {
-          50: '#ECEDEE',
-          100: '#DCDDDE',
-          200: '#B9BBBE',
-          300: '#8E9297',
-          400: '#72767D',
-          500: '#5C6067',
-          550: '#4F545C',
-          600: '#464950',
-          700: '#36393F',
-          800: '#2F3136',
-          900: '#202225',
-        }
-      }
+          50: "#ECEDEE",
+          100: "#DCDDDE",
+          200: "#B9BBBE",
+          300: "#8E9297",
+          400: "#72767D",
+          500: "#5C6067",
+          550: "#4F545C",
+          600: "#464950",
+          700: "#36393F",
+          800: "#2F3136",
+          900: "#202225",
+        },
+      },
     },
   },
-  plugins: [],
-}
-
+  // eslint-disable-next-line no-undef
+  plugins: [require("@tailwindcss/forms")],
+};
