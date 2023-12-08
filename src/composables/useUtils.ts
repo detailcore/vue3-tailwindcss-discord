5deceee90ffb63cc813d4daa66e02831e23146ac
detailcore <savemysoulok@yandex.ru>
2023-12-08 12:20:38 +0400

    Update useUtils.ts

--- src/composables/useUtils.ts
+++ src/composables/useUtils.ts
@@ -1,6 +1,6 @@
 export const useUtils = () => {
   function getImageUrl(path: string, name: string): string {
-    const dir = import.meta.env.PROD ? '' : '../../public/'
+    const dir = import.meta.env.PROD ? '../' : '../../public/'
     return new URL(dir + path + '/' + name, import.meta.url).href
   }
 
